872a2270ead95cafec876f86832352c9c89bec12
Kitsanapong Roubgome <kitsanapong.r@gmail.com>
2020-10-24 15:12:49 +0700

    mock control panel dom

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,33 @@
 import Head from 'next/head'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+function Score(props) {
+  const { name = '', value = '-'} = props
+  return (
+    <div className="score click-number d-flex flex-column">
+      <div className="name">{name}</div>
+      <div className="value">{value}</div>
+    </div>
+  )
+}
+
+function Button() {
+  return (
+    <div>New Game</div>
+  )
+}
+
+function ControlPanel() {
+  return (
+    <div className="control-panel d-flx flex-column">
+      <Score name="Click"/>
+      <Score name="My Best"/>
+      <Score name="Global Best"/>
+      <Button/>
+    </div>
+  )
+}
+
 export default function Home() {
   return (
     <div>
@@ -8,10 +35,9 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <div className="d-flex flex-row">
-          <div>Hello</div>
-          <div>Hello</div>
+      <main className="container">
+        <div className=" d-flex flex-row">
+          <ControlPanel/>
         </div>
       </main>
     </div>
